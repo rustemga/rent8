822f0dca174e77d4806f7194a3fe5298a2ca0c36
Rustem.Garaev <Tel52093547!@>
2020-10-07 17:28:56 +0300

    адаптивность инпута

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                 <label for="email"
                        class="text-xl font-hairline text-gray-700 mb-2 block">{{ __('E-Mail Address') }}</label>
                 <input
-                    class="shadow w-1/5 px-4 py-2 border-red-600 text-gray-700 text-xl mb-6 mt-2 rounded-lg focus:outline-none"
+                    class="shadow xl:w-1/5 lg:w-1/3 md:w-1/3 sm:w-1/2 px-4 py-2 border-red-600 text-gray-700 text-xl mb-6 mt-2 rounded-lg focus:outline-none"
                     type="email" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                 @error('email')
                 <span class="invalid-feedback" role="alert">
