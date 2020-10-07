1399ec5892e10a4970e29ad3c3cf1626d2c2a437
Rustem.Garaev <Tel52093547!@>
2020-10-07 15:06:35 +0300

    rent8 тест коммит с работы

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
             @endif
             <div class="text-center">
                 <img class="w-1/6 rounded-lg shadow-xl mx-auto" src="\img\logo.jpg" alt="logo">
-                <h1 class="text-4xl font-hairline text-red-600 mt-10 mb-10"><i class="fas fa-stream mr-6"></i>В прокате</h1>
+                <h1 class="text-4xl font-hairline text-red-600 mt-10 mb-10"><i class="fas fa-stream mr-6"></i>В прокатеe</h1>
             </div>
             <div class="flex flex-wrap mx-auto bg-red-600 px-20 py-10">
                 <div class="bg-gray-100 px-4 py-6 mx-6 mb-6 flex-initial text-center w-1/6 hover:bg-gray-300 shadow-xl">
