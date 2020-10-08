f9a3ffa4525d3da1d3e953c5b2d5c6cbfc1b6571
Rustem.Garaev <Tel52093547!@>
2020-10-08 17:06:58 +0300

    создал home страницы

---
+++ resources/views/homeAdmin.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="p-8 text-center">
+        <h1 class="text-4xl font-hairline text-red-600"><i class="fas fa-house-user mr-6 shadow-lg"></i>Личный кабинет</h1>
+        <p>Администратор</p>
+    </div>
+    <div class="p-8 text-center">
+        <div>
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+        </div>
+    </div>
+
+
+
+@endsection
